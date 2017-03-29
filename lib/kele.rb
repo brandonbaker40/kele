c04f7197cdd2badd8048d8a063b059628738a99a
Brandon Baker <brandonbaker40@icloud.com>
2017-03-29 19:31:39 -0400

    get mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,5 +1,5 @@
 require 'httparty'
-#require 'json'
+require 'json'
 
 class Kele
 
@@ -7,15 +7,22 @@ class Kele
 
   def initialize(email, password)
     @api_url = 'https://www.bloc.io/api/v1'
-    response = self.class.post(@api_url + '/sessions', body: {"email": email, "password": password})
+    response = self.class.post(@api_url + "/sessions", body: {"email": email, "password": password})
     puts "Success!"
     raise "Invalid credentials. Please try again!" if response.code != 200
     @auth_token = response["auth_token"]
   end
 
   def get_me
-    response = self.class.get(@api_url + '/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get(@api_url + "/users/me", headers: { "authorization" => @auth_token })
     @current_user = JSON.parse(response.body)
   end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(@api_url + "/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
+  end
+
+
 
 end
