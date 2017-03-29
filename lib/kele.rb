54ff8881bfd32530eb310f2b4f08c70e58b4d0ad
Brandon Baker <brandonbaker40@icloud.com>
2017-03-29 09:25:07 -0400

    retrieves user

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+#require 'json'
 
 class Kele
 
@@ -10,6 +11,11 @@ class Kele
     puts "Success!"
     raise "Invalid credentials. Please try again!" if response.code != 200
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(@api_url + '/users/me', headers: { "authorization" => @auth_token })
+    @current_user = JSON.parse(response.body)
   end
 
 end
