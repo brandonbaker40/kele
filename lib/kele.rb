103d54239629fbc8041682c00a6e9a6887b80f14
Brandon Baker <brandonbaker40@icloud.com>
2017-04-07 11:02:59 -0400

    creates submissions on checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -35,4 +35,9 @@ class Kele
     @create_message = response["success"]
   end
 
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post(@api_url + "/checkpoint_submissions", body: {"assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "checkpoint_id": checkpoint_id, "comment": comment, "enrollment_id": 24370}, headers: { "authorization" => @auth_token })
+    @create_submission = response["enrollment_id"]
+  end
+
 end
